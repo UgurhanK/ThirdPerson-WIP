ff7041e20468d80fe55f7d768b62de8606a29bca
UgurhanK <105857708+UgurhanK@users.noreply.github.com>
2024-04-08 15:08:27 +0300

    Add files via upload

---
+++ ThirdPerson/EntityUtilities.cs
@@ -0,0 +1,96 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Admin;
+using CounterStrikeSharp.API.Modules.Commands;
+using CounterStrikeSharp.API.Modules.Entities;
+using CounterStrikeSharp.API.Modules.Utils;
+using System.Drawing;
+
+namespace ThirdPerson;
+
+public static class EntityUtilities
+{
+    static public void SetColor(this CDynamicProp? prop, Color colour)
+    {
+        if (prop != null && prop.IsValid)
+        {
+            prop.Render = colour;
+            Utilities.SetStateChanged(prop, "CBaseModelEntity", "m_clrRender");
+        }
+    }
+
+    static public void SetColor(this CPhysicsPropMultiplayer? prop, Color colour)
+    {
+        if (prop != null && prop.IsValid)
+        {
+            prop.Render = colour;
+            Utilities.SetStateChanged(prop, "CBaseModelEntity", "m_clrRender");
+        }
+    }
+
+    public static void UpdateCamera(this CDynamicProp _cameraProp, CCSPlayerController target)
+    {
+        _cameraProp.Teleport(target.CalculatePositionInFront(-110, 90), target.PlayerPawn.Value!.V_angle, new Vector());
+    }
+
+    public static void UpdateCameraSmooth(this CPhysicsPropMultiplayer _cameraProp, CCSPlayerController target)
+    {
+        Vector velocity = CalculateVelocity(_cameraProp.AbsOrigin!, target.CalculatePositionInFront(-110, 90), ThirdPerson.cfg.SmoothDuration);
+        _cameraProp.Teleport(_cameraProp.AbsOrigin!, target.PlayerPawn.Value!.V_angle, velocity);
+    }
+
+    public static Vector CalculateVelocity(Vector positionA, Vector positionB, float timeDuration)
+    {
+        // Step 1: Determine direction from A to B
+        Vector directionVector = positionB - positionA;
+
+        // Step 2: Calculate distance between A and B
+        float distance = directionVector.Length();
+
+        // Step 3: Choose a desired time duration for the movement
+        // Ensure that timeDuration is not zero to avoid division by zero
+        if (timeDuration == 0)
+        {
+            timeDuration = 1;
+        }
+
+        // Step 4: Calculate velocity magnitude based on distance and time
+        float velocityMagnitude = distance / timeDuration;
+
+        // Step 5: Normalize direction vector
+        if (distance != 0)
+        {
+            directionVector /= distance;
+        }
+
+        // Step 6: Scale direction vector by velocity magnitude to get velocity vector
+        Vector velocityVector = directionVector * velocityMagnitude;
+
+        return velocityVector;
+    }
+
+    public static Vector CalculatePositionInFront(this CCSPlayerController player, float offSetXY, float offSetZ = 0)
+    {
+        var pawn = player.PlayerPawn.Value;
+        // Extract yaw angle from player's rotation QAngle
+        float yawAngle = pawn!.EyeAngles!.Y;
+
+        // Convert yaw angle from degrees to radians
+        float yawAngleRadians = (float)(yawAngle * Math.PI / 180.0);
+
+        // Calculate offsets in x and y directions
+        float offsetX = offSetXY * (float)Math.Cos(yawAngleRadians);
+        float offsetY = offSetXY * (float)Math.Sin(yawAngleRadians);
+
+        // Calculate position in front of the player
+        var positionInFront = new Vector
+        {
+            X = pawn!.AbsOrigin!.X + offsetX,
+            Y = pawn!.AbsOrigin!.Y + offsetY,
+            Z = pawn!.AbsOrigin!.Z + offSetZ
+        };
+
+        return positionInFront;
+    }
+}
---
+++ ThirdPerson/ThirdPerson.cs
@@ -0,0 +1,181 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Admin;
+using CounterStrikeSharp.API.Modules.Commands;
+using CounterStrikeSharp.API.Modules.Entities;
+using CounterStrikeSharp.API.Modules.Entities.Constants;
+using CounterStrikeSharp.API.Modules.Utils;
+using System.Drawing;
+using System.Text.Json.Serialization;
+
+namespace ThirdPerson 
+{
+    public class ThirdPerson : BasePlugin, IPluginConfig<Config>
+    {
+        public override string ModuleName => "ThirdPerson";
+        public override string ModuleVersion => "0.0.2";
+        public override string ModuleAuthor => "BoinK & UgurhanK";
+        public override string ModuleDescription => "Basic Third Person";
+
+        public Config Config { get; set; } = null!;
+        public static Config cfg;
+        public void OnConfigParsed(Config config) { Config = config; cfg = config; }
+
+        public static Dictionary<CCSPlayerController, CDynamicProp> thirdPersonPool = new Dictionary<CCSPlayerController, CDynamicProp>();
+        public static Dictionary<CCSPlayerController, CPhysicsPropMultiplayer> smoothThirdPersonPool = new Dictionary<CCSPlayerController, CPhysicsPropMultiplayer>();
+
+        public override void Load(bool hotReload)
+        {
+            RegisterListener<Listeners.OnTick>(OnGameFrame);
+            RegisterEventHandler<EventRoundStart>(OnRoundStart);
+
+            AddCommand("css_tp", "Allows to use thirdperson", OnTPCommand);
+            AddCommand("css_thirdperson", "Allows to use thirdperson", OnTPCommand);
+        }
+
+        public void OnGameFrame()
+        {
+            foreach (var player in thirdPersonPool.Keys)
+            {
+                thirdPersonPool[player].UpdateCamera(player);
+            }
+
+            foreach (var player in smoothThirdPersonPool.Keys)
+            {
+                smoothThirdPersonPool[player].UpdateCameraSmooth(player);
+            }
+        }
+
+        private HookResult OnRoundStart(EventRoundStart @event, GameEventInfo info)
+        {
+            thirdPersonPool.Clear();
+            smoothThirdPersonPool.Clear();
+            return HookResult.Continue;
+        }
+
+        public void OnTPCommand(CCSPlayerController? caller, CommandInfo command)
+        {
+            if(Config.UseOnlyAdmin && !AdminManager.PlayerHasPermissions(caller, Config.Flag))
+            {
+                command.ReplyToCommand(ReplaceColorTags(Config.NoPermission));
+                return;
+            }
+
+            if (caller == null || !caller.PawnIsAlive) return;
+
+            if (Config.UseSmooth)
+            {
+                SmoothThirdPerson(caller);
+            } 
+            else
+            {
+                DefaultThirdPerson(caller);
+            }
+        }
+
+        public void DefaultThirdPerson(CCSPlayerController caller)
+        {
+            if (!thirdPersonPool.ContainsKey(caller))
+            {
+                CDynamicProp? _cameraProp = Utilities.CreateEntityByName<CDynamicProp>("prop_dynamic");
+
+                if (_cameraProp == null) return;
+
+                _cameraProp.DispatchSpawn();
+                _cameraProp.SetColor(Color.FromArgb(0, 255, 255, 255));
+
+                _cameraProp.Teleport(caller.CalculatePositionInFront(-110, 90), caller.PlayerPawn.Value!.V_angle, new Vector());
+
+                caller.PlayerPawn!.Value!.CameraServices!.ViewEntity.Raw = _cameraProp.EntityHandle.Raw;
+                Utilities.SetStateChanged(caller.PlayerPawn!.Value!, "CBasePlayerPawn", "m_pCameraServices");
+
+                caller.PrintToChat(ReplaceColorTags(Config.Prefix + Config.OnActivated));
+
+                thirdPersonPool.Add(caller, _cameraProp);
+
+                AddTimer(0.5f, () =>
+                {
+                    _cameraProp.Teleport(caller.CalculatePositionInFront(-110, 90), caller.PlayerPawn.Value.V_angle, new Vector());
+                });
+
+            }
+            else
+            {
+                caller!.PlayerPawn!.Value!.CameraServices!.ViewEntity.Raw = uint.MaxValue;
+                AddTimer(0.3f, () => Utilities.SetStateChanged(caller.PlayerPawn!.Value!, "CBasePlayerPawn", "m_pCameraServices"));
+                if (thirdPersonPool[caller] != null && thirdPersonPool[caller].IsValid) thirdPersonPool[caller].Remove();
+                caller.PrintToChat(ReplaceColorTags(Config.Prefix + Config.OnDeactivated));
+                thirdPersonPool.Remove(caller);
+            }
+        }
+
+        public void SmoothThirdPerson(CCSPlayerController caller)
+        {
+            if (!smoothThirdPersonPool.ContainsKey(caller))
+            {
+
+                var _cameraProp = Utilities.CreateEntityByName<CPhysicsPropMultiplayer>("prop_physics_multiplayer");
+
+                if (_cameraProp == null || !_cameraProp.IsValid) return;
+
+                _cameraProp.DispatchSpawn();
+
+                _cameraProp.Collision.CollisionGroup = (byte)CollisionGroup.COLLISION_GROUP_NEVER;
+                _cameraProp.Collision.SolidFlags = 12;
+                _cameraProp.Collision.SolidType = SolidType_t.SOLID_VPHYSICS;
+
+                _cameraProp.SetColor(Color.FromArgb(0, 255, 255, 255));
+
+                //Changes players view to camera prop- ViewEntity Raw value can be set to uint.MaxValue to change back to normal player cam
+                caller.PlayerPawn.Value!.CameraServices!.ViewEntity.Raw = _cameraProp.EntityHandle.Raw;
+                Utilities.SetStateChanged(caller.PlayerPawn.Value, "CBasePlayerPawn", "m_pCameraServices");
+
+                _cameraProp.Teleport(caller.CalculatePositionInFront(-110, 90), caller.PlayerPawn.Value.V_angle, new Vector());
+
+                smoothThirdPersonPool.Add(caller, _cameraProp);
+
+                caller.PrintToChat(ReplaceColorTags(Config.Prefix + Config.OnActivated));
+            }
+            else
+            {
+                caller!.PlayerPawn!.Value!.CameraServices!.ViewEntity.Raw = uint.MaxValue;
+                AddTimer(0.3f, () => Utilities.SetStateChanged(caller.PlayerPawn!.Value!, "CBasePlayerPawn", "m_pCameraServices"));
+                if (smoothThirdPersonPool[caller] != null && smoothThirdPersonPool[caller].IsValid) smoothThirdPersonPool[caller].Remove();
+                caller.PrintToChat(ReplaceColorTags(Config.Prefix + Config.OnDeactivated));
+                smoothThirdPersonPool.Remove(caller);
+            }
+        }
+
+        public string ReplaceColorTags(string input)
+        {
+
+        string[] colorPatterns =
+            {
+            "{DEFAULT}", "{DARKRED}", "{LIGHTPURPLE}", "{GREEN}", "{OLIVE}", "{LIME}", "{RED}", "{GREY}",
+            "{YELLOW}", "{SILVER}", "{BLUE}", "{DARKBLUE}", "{ORANGE}", "{PURPLE}"
+        };
+            string[] colorReplacements =
+            {
+            "\x01", "\x02", "\x03", "\x04", "\x05", "\x06", "\x07", "\x08", "\x09", "\x0A", "\x0B", "\x0C", "\x10", "\x0E"
+        };
+
+            for (var i = 0; i < colorPatterns.Length; i++)
+                input = input.Replace(colorPatterns[i], colorReplacements[i]);
+
+            return input;
+        }
+    }
+
+    public class Config : BasePluginConfig
+    {
+        [JsonPropertyName("OnActivated")] public string OnActivated { get; set; } = "Third Person Activated";
+        [JsonPropertyName("OnDeactivated")] public string OnDeactivated { get; set; } = "Third Person Deactivated";
+        [JsonPropertyName("NoPermission")] public string NoPermission { get; set; } = "You dont have to access this command.";
+        [JsonPropertyName("Prefix")] public string Prefix { get; set; } = " [ {DARKRED}Third Person {DEFAULT}] ";
+        [JsonPropertyName("UseOnlyAdmin")] public bool UseOnlyAdmin { get; set; } = false;
+        [JsonPropertyName("OnlyAdminFlag")] public string Flag { get; set; } = "@css/slay";
+        [JsonPropertyName("UseSmoothCam")] public bool UseSmooth { get; set; } = false;
+        [JsonPropertyName("SmoothCamDuration")] public float SmoothDuration { get; set; } = 0.01f;
+    }
+}
